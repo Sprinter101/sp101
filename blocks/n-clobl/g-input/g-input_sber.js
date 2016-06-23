6a07aaafbc32dc0407b9dde80bba4ae2e1dab377
Arzach <ruslan--47@yandex.ru>
2016-06-23 14:59:42 +0300

    SV-33 Added error message box + did lots of fixes

--- blocks/n-clobl/g-input/g-input_sber.js
+++ blocks/n-clobl/g-input/g-input_sber.js
@@ -17,12 +17,24 @@ sv.gInput.Input = function(view, opt_params, opt_domHelper) {
     goog.base(this, view, opt_params, opt_domHelper);
 
     /**
+     * Possible validation type handlers
+     * @type {Object}
+     */
+    this.validationTypeHandlers = {
+        'digits': this.validateDigits_,
+        'email': this.validateEmail_,
+        'notEmpty': this.validateNotEmpty_,
+        'maxDonation': this.validateMaxDonation_
+    };
+
+    /**
      * Possible constraint type handlers
      * @type {Object}
      */
     this.constraintsHandlers = {
-        'digitsOnly': this.constraintDigitsOnly_
-    }
+        'digitsOnly': this.constraintDigitsOnly_,
+        'charactersLimit': this.constraintCharactersLimit_
+    };
 
 };
 goog.inherits(sv.gInput.Input, cl.gInput.Input);
@@ -44,6 +56,16 @@ goog.scope(function() {
     };
 
     /**
+     * Const enum
+     * @enum {number}
+     */
+    Input.Const = {
+        MAX_DONATION_AMOUNT: 500000,
+        CHARACTERS_LIMIT: 6
+    };
+
+
+    /**
      * @override
      */
     Input.prototype.enterDocument = function() {
@@ -63,6 +85,39 @@ goog.scope(function() {
     };
 
     /**
+     * Validate input depends of it type
+     * @return {boolean}
+     */
+    Input.prototype.validate = function() {
+        var that = this,
+            failedValidations;
+
+        /** Check all validations, that comes from params
+         and add failed validations to array of failed validations **/
+        failedValidations = this.params['validations'].filter(
+            function(validationType) {
+                return that.doValidationType_(validationType);
+            }
+        );
+
+        /** If array of failed validations is empty => input is valid **/
+        var isValid = !failedValidations.length;
+        if (isValid) {
+            this.getView().unSetNotValidState();
+            this.getView().hideErrorMessage();
+        } else {
+            this.getView().setNotValidState();
+            this.getView().showErrorMessage(failedValidations);
+
+            this.dispatchEvent({
+                'type': Input.Event.NOT_VALID,
+                'failedValidations': failedValidations
+            });
+        }
+        return isValid;
+    };
+
+    /**
      * Input handler
      */
     Input.prototype.onInput = function() {
@@ -71,11 +126,13 @@ goog.scope(function() {
         this.params['constraints'].forEach(function(constraintType) {
                 that.doConstraintType_(constraintType);
         });
-        
+
     };
 
     /**
-    * Executes a constraint of given type and assigns the returned string to the input value
+    * Executes a constraint of given type
+    * and assigns the returned string to the input value
+    * @private
     * @param {string} constraintType
     */
     Input.prototype.doConstraintType_ = function(constraintType) {
@@ -85,15 +142,53 @@ goog.scope(function() {
         var newValue_ = constraintFunction(oldValue_);
 
         this.setValue(newValue_);
-    }
+    };
 
     /**
      * Removes all non-numeric characters from the string
+     * @private
      * @param {string} oldValue
      * @return {boolean}
      */
     Input.prototype.constraintDigitsOnly_ = function(oldValue) {
         return oldValue.replace(/[\D]/g, '');
-    }
+    };
+
+    /**
+     * Removes all extra characters
+     * @private
+     * @param {string} oldValue
+     * @return {boolean}
+     */
+    Input.prototype.constraintCharactersLimit_ = function(oldValue) {
+        return oldValue.slice(0, Input.Const.CHARACTERS_LIMIT);
+    };
+
+    /**
+     * Validate digit
+     * @param {string} text text to validate
+     * @return {boolean}
+     * @private
+     */
+    Input.prototype.validateDigits_ = function(text) {
+
+        if (!text) {
+            return true;
+        }
+
+        var digitRegex = /^\d+$/;
+        return digitRegex.test(text);
+    };
+
+    /**
+     * Validate digit
+     * @param {string} text text to validate
+     * @return {boolean}
+     * @private
+     */
+    Input.prototype.validateMaxDonation_ = function(text) {
+        var donationAmount = Number(text);
+        return !(donationAmount > Input.Const.MAX_DONATION_AMOUNT);
+    };
 
 });  // goog.scope
