d72c51f48258ddfc46c14114e9726ff945afaf55
Elena Sufieva <fiolent06@gmail.com>
2016-06-23 14:32:56 +0300

    SV-32 deleted empty g-icon js files
    Kept only images,soy and scss

--- blocks/n-clobl/i-factory/i-factory_sber.js
+++ blocks/n-clobl/i-factory/i-factory_sber.js
@@ -3,8 +3,6 @@ goog.provide('sv.iFactory.FactorySber');
 goog.require('cl.iFactory.Factory');
 goog.require('sv.gButton.Button');
 goog.require('sv.gButton.View');
-goog.require('sv.gIcon.Icon');
-goog.require('sv.gIcon.View');
 goog.require('sv.gTestPage.TestPage');
 goog.require('sv.gTestPage.View');
 goog.require('sv.iFactory.TemplateFactory');
@@ -56,10 +54,6 @@ sv.iFactory.FactorySber = function() {
         .setControlListItem('ButtonSber', {
             control: sv.gButton.Button,
             view: sv.gButton.View
-        })
-        .setControlListItem('IconSber', {
-            control: sv.gIcon.Icon,
-            view: sv.gIcon.View
         });
 };
 goog.inherits(sv.iFactory.FactorySber, cl.iFactory.Factory);
